dc8eb3e5cd957da22046c255df687062d1b723ac
ahendler99 <axehen-1@student.ltu.se>
2022-05-15 19:01:09 +0200

    Error, Exception in tread main

---
+++ src/main/java/SeleniumTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+    static final String DRIVER_TYPE = "webdriver.chrome.driver";
+    static final String DRIVER_FILE = "/Users/axelhendler/Desktop/chromedriver 3";
+
+    public static void main(String[] args) {
+        System.setProperty(DRIVER_TYPE, DRIVER_FILE);
+        WebDriver driver = new ChromeDriver();
+
+        String baseUrl = "https://www.google.com/";
+        driver.get(baseUrl);
+        System.out.println(driver.getTitle());
+        driver.manage().deleteAllCookies();
+        driver.get("https://www.google.com/");
+
+        driver.manage().deleteAllCookies();
+        driver.close();
+        driver.quit();
+    }
+}
